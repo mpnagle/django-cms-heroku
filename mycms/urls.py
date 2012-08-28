117122ceca94367bf023f6ac139f2ca17e9ba772
Larisa Berger <larisa@org.org>
2012-08-27 22:09:52 -0400

    WE HAVE AN OUTLINE PLUGIN OMG

--- mycms/urls.py
+++ mycms/urls.py
@@ -5,7 +5,13 @@ from django.conf import settings
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    (r'^admin/', include(admin.site.urls)),
-    (r'^', include('cms.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^', include('cms.urls')),
 )
 
+
+urlpatterns = patterns('',
+    url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
+    {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
+    url(r'', include('django.contrib.staticfiles.urls')),
+) + urlpatterns
