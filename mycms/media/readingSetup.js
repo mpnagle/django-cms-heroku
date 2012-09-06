7e1fd6c08c2fdd7a27344fa33ae3fdef5c65345e
Larisa Berger <larisa@org.org>
2012-09-06 19:20:19 -0400

    trying to match ref to footNumber instead of sup

--- mycms/media/readingSetup.js
+++ mycms/media/readingSetup.js
@@ -120,7 +120,7 @@ $('.references').each(function(i){
     if (i==0){
         return true;
     }
-    var footMatch = $($('footNumber')[i]);
+    var footMatch = $($('.footNumber')[i]);
     console.log('this offset');
     console.log($(this).offset());
     console.log('footMatch');
