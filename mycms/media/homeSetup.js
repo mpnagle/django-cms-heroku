9681981c8a637ede379d8c5274872de60cf8a152
Larisa Berger <larisa@org.org>
2012-09-09 17:59:19 -0400

    added debug prints to homesetup.js

--- mycms/media/homeSetup.js
+++ mycms/media/homeSetup.js
@@ -7,6 +7,7 @@ console.log(screenWidth);
 
 //position globe image (width = 550px)
 var globeLeft = (screenWidth/3)-550;
+console.log('setting Globe Position to ' + globeLeft);
 $('#globe').css('left', globeLeft);
 
 /*
