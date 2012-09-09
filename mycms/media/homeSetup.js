443cdd2ac754d5ecda5eb637d21d25849e8900e2
Larisa Berger <larisa@org.org>
2012-09-09 17:53:00 -0400

    positioning globe on homepage, updated footnote colors to amplify color.

--- mycms/media/homeSetup.js
+++ mycms/media/homeSetup.js
@@ -4,14 +4,18 @@
 var screenWidth = $(window).width();
 console.log('screenWidth');
 console.log(screenWidth);
+
+//position globe image (width = 550px)
+var globeLeft = (screenWidth/3)-550;
+$('#globe').css('left', globeLeft);
+
+/*
 var totalWidth = $('#amplifyHeading').outerWidth() + $('#innovations').width();
 console.log('totalWidth amplify + innovations');
 console.log(totalWidth);
-$('#amplifyHeading').css('left', (screenWidth-totalWidth)/2);
-$('#innovations').css('left', $('#amplifyHeading').position().left+$('#amplifyHeading').outerWidth());
 
-/*
-<div id=africa>
-    <canvas data-processing-sources="africaFinal.pde"></canvas>
-</div>
+$('#amplifyHeading').css('left', (screenWidth-totalWidth)/2);
+
+
+$('#innovations').css('left', $('#amplifyHeading').position().left+$('#amplifyHeading').outerWidth());
 */
