1397c4c59f17f728b23c3a04d72778cc436ab6bd
Larisa Berger <larisa@org.org>
2012-09-10 19:31:49 -0400

    messed with homepage alignment, copied stuff from reading page.

--- mycms/media/homeSetup.js
+++ mycms/media/homeSetup.js
@@ -4,12 +4,18 @@ positionHome();
 
 function positionHome(){
     //position amplify and innovations
+    $('#amplifyHeading').css('padding-left', '50px');
+    $('#innovationsWholeDiv').css('left', $('#amplifyHeading').position().left + $('#amplifyHeading').outerWidth());
+    $('#innovationsWholeDiv').css('top', $('#amplifyHeading').position().top);
+
+    
+
     var screenWidth = $(window).width();
     console.log('screenWidth');
     console.log(screenWidth);
 
     //position globe image (width = 550px)
-    var mapLeft = (screenWidth/3)-550;
+    var mapLeft = (screenWidth*(1/2));
 
 
     console.log('setting Globe Position to ' +mapLeft);
