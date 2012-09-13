d338ce71346b6ebeccf849321e1d9f88ed887857
Larisa Berger <larisa@org.org>
2012-09-13 15:29:08 -0400

    added hover to home, changes opacity.

--- mycms/media/homeSetup.js
+++ mycms/media/homeSetup.js
@@ -2,11 +2,17 @@ $(document).ready(function() {
     $('#homeSlate').height($('#innovIndex').outerHeight()+$('#amplifyHome').outerHeight()+$('.volHeading').height());
     $('#amplifySpiel').height($('#homeSlate').height()-$('#amplifyHome').outerHeight());
 
-    $('.homeArtTitle').each(function(i){
-        console.log('about to fade in');
-        $(this).fadeIn('slow');
+    $('.homeArtTitle').hover(
+        function(){            
+            console.log('about to fade in');
+            $(this).css({opacity:1.0});
+        },
+        function(){
+            console.log('about to fade out');
+            $(this).css({opacity:0.5});
+        }
+        );
 
-    });
  
 /*
 var totalWidth = $('#amplifyHeading').outerWidth() + $('#innovations').width();
