bc8a8da0edeae121c7998762c8fafed10fa3be4a
Larisa Berger <larisa@org.org>
2012-09-13 16:45:12 -0400

    finally got the opacity doing tricks, after help from alec.

--- mycms/media/homeSetup.js
+++ mycms/media/homeSetup.js
@@ -2,18 +2,43 @@ $(document).ready(function() {
     $('#homeSlate').height($('#innovIndex').outerHeight()+$('#amplifyHome').outerHeight()+$('.volHeading').height());
     $('#amplifySpiel').height($('#homeSlate').height()-$('#amplifyHome').outerHeight());
 
-    $('.homeArtTitle').hover(
-        function(){            
-            console.log('about to fade in');
-            $(this).css({opacity:1.0});
-        },
-        function(){
-            console.log('about to fade out');
-            $(this).css({opacity:0.5});
-        }
-        );
+   $('.homeArtTitle').hover(
 
- 
+       function() { 
+           categoryLi = $(this).closest('li.innovCategory');
+           categorySpan = categoryLi.children('span');
+           categorySpan.css({opacity: 1.0});
+           volLi = $(this).closest('li.volHeading');
+           volSpan = volLi.children('span');
+           volSpan.css({opacity: 1.0});
+       },
+
+       function() { 
+           categoryLi = $(this).closest('li.innovCategory');
+           categorySpan = categoryLi.children('span');
+           categorySpan.css({opacity: 0.35});
+           volLi = $(this).closest('li.volHeading');
+           volSpan = volLi.children('span');
+           volSpan.css({opacity: 0.35});
+       }
+       );
+
+    // function(){
+    //     $(this).css({opacity: 0.5});
+    //     var currCategory = $(this).closest('.innovCategory');
+    //     currCategory.css({opacity: 0.5});
+    //     var currIssue = $(this).closest('.volHeading');
+    //     $(currIssue).css({opacity: 0.5});
+    // },
+    // function(){
+    //     var currCategory = $(this).closest('.innovCategory');
+    //     $(currCategory).css({opacity: 0.5});
+    //     var currIssue = $(this).closest('.volHeading');
+    //     $(currIssue).css({opacity: 0.5});
+    // }
+//  );
+        
+
 /*
 var totalWidth = $('#amplifyHeading').outerWidth() + $('#innovations').width();
 
